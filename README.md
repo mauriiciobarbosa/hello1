e6a2e8549686d403a0e910abfe5b8e933800f508
Mauricio Barbosa <mauricio@mauricio-PC01.1>
2015-07-20 22:38:55 -0300

    First Commit =)

---
+++ README.md
@@ -0,0 +1,3 @@
+The hello1 application is a web module that uses JavaServer Faces technology to display a greeting and response.
+
+For more information: https://docs.oracle.com/javaee/7/tutorial/webapp003.htm
---
+++ src/hello1/Hello.java
@@ -0,0 +1,24 @@
+package hello1;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class Hello {
+
+	private String name;
+	
+	public Hello() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
